e53d299d78850624f57a58c2595652aa620b5031
Krister <krister@Kristers-MacBook-Pro.local>
2022-09-27 23:18:08 +0300

    Basic structure from Juhos Figma

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const {
 } = require('express-handlebars');
 const axios = require('axios').default;
 const MarkdownIt = require('markdown-it');
-
 const markdown = new MarkdownIt();
 
 const app = express();
@@ -14,6 +13,11 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.get('/', (req, res) => {
+  res.render('menulist');
+});
+
+/*
 app.get('/', (req, res) => {
   axios.get('https://raw.githubusercontent.com/tluhk/Riistvara-ja-operatsioonisysteemide-alused-HKI5085.HK/main/README.md')
     .then((response) => {
@@ -25,17 +29,17 @@ app.get('/', (req, res) => {
 
       // Trying to split by markdown headings and render each part individually:
 
-      // const resultsSplit = results.match(/^#+ [^#]*(?:#(?!#)[^#]*)*/gm);
+      // const resultsSplit = results.match(/^#+ [^#]*(?:#(?!#)[^#]*)*\/gm);
       /* resultsSplit.forEach(element => {
       });
       console.log(resultsSplit);
       console.log(resultsSplit.length);
-      res.render('home', { readme: resultsSplit, markdown }); */
+      res.render('home', { readme: resultsSplit, markdown });
     })
     .catch((error) => {
       console.log(error);
     });
-});
+}); */
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
