a5514a31d7d512ef2b446d9bfc60f62f87f76b4d
Krister <krister@Kristers-MacBook-Pro.local>
2022-10-29 17:26:40 +0300

    disable console logs

--- functions/updateRepoJSONFile.js
+++ functions/updateRepoJSONFile.js
@@ -6,13 +6,13 @@ const file = require(fileName);
 
 const updateRepoJSONFile = (name) => {
   file.name = name;
-  console.log('name', name);
-  console.log('file.name', file.name);
+  // console.log('name', name);
+  // console.log('file.name', file.name);
 
   fs.writeFile(fileName, JSON.stringify(file, null, 2), (err) => {
     if (err) return console.log(err);
-    console.log(JSON.stringify(file));
-    console.log(`writing to ${fileName}`);
+    // console.log(JSON.stringify(file));
+    // console.log(`writing to ${fileName}`);
 
     return true;
   });
