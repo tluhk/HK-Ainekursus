ba9484b683c177d418c02b8b5f089f70f496c91d
Juho Kalberg <juss@vilenkell.eu>
2022-11-03 21:01:33 +0200

    Added mobile closing top-menu

--- public/js/js.js
+++ public/js/js.js
@@ -2,3 +2,7 @@
 // document
 //  .querySelectorAll('#sidebar a, #sidebar h3')
 //  .forEach((el) => (el.innerText = el.innerText.split('_').join(' ')));
+function closeMenu() {
+  let x = document.querySelector('.closing-menu');
+  x.classList.toggle('top-zero');
+}
