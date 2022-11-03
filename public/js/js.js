5be8070a9be09371f3ec3be9fdc993036d3f4ad4
Juho Kalberg <juss@vilenkell.eu>
2022-11-03 21:53:14 +0200

    added sidebar sliding mobile menu

--- public/js/js.js
+++ public/js/js.js
@@ -4,5 +4,10 @@
 //  .forEach((el) => (el.innerText = el.innerText.split('_').join(' ')));
 function closeMenu() {
   let x = document.querySelector('.closing-menu');
-  x.classList.toggle('top-zero');
+  x.classList.toggle('menu-zero');
+}
+
+function closeSideMenu() {
+  let x = document.querySelector('.sidebar');
+  x.classList.toggle('menu-zero');
 }
