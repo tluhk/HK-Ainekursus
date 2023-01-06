25c1878309fec806f201df2bb4a9365ce63d060f
Juho Kalberg <juss@vilenkell.eu>
2023-01-06 22:01:12 +0200

    prod

--- app.js
+++ app.js
@@ -11,21 +11,6 @@ const helmet = require('helmet');
 
 const PORT = process.env.PORT || 3000;
 
-/* const livereload = require('livereload');
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, '/views'));
-liveReloadServer.watch(path.join(__dirname, 'public'));
-liveReloadServer.server.once('connection', () => {
-  setTimeout(() => {
-    liveReloadServer.refresh('/');
-  }, 100);
-});
-const connectLivereload = require('connect-livereload');
-
-app.use(connectLivereload()); */
-
-// add handlebars helpers: https://stackoverflow.com/a/32707476
 const handlebars = require('./src/helpers/handlebars')(exphbs);
 
 app.use(compression());
@@ -35,7 +20,6 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/views'));
 
-// define application static folder:
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(
   '/images',
@@ -51,15 +35,3 @@ engine(app);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-/* const start = (portProp) => {
-  try {
-    app.listen(portProp);
-    console.log(`Listening on port ${portProp}`);
-  } catch (err) {
-    console.error(err);
-    process.exit();
-  }
-};
-
-start(port); */
