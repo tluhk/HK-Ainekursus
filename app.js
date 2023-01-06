64bb8c6dcde524f2b9048347eb72d210ef30d5aa
Juho Kalberg <juss@vilenkell.eu>
2023-01-06 21:52:44 +0200

    comment out livereload

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const compression = require('compression');
 const helmet = require('helmet');
 const PORT = process.env.PORT || 3000;
 
+/* const livereload = require('livereload');
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, '/views'));
+liveReloadServer.watch(path.join(__dirname, 'public'));
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+const connectLivereload = require('connect-livereload');
+app.use(connectLivereload()); */
+
 // add handlebars helpers: https://stackoverflow.com/a/32707476
 const handlebars = require('./src/helpers/handlebars')(exphbs);
 
