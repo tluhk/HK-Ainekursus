ac490ee1739c1e1d34ccb3ab982e5a58e13b4be6
Juho Kalberg <juss@vilenkell.eu>
2023-01-06 21:45:24 +0200

    Added compression and helmet for security

--- app.js
+++ app.js
@@ -7,24 +7,28 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const compression = require('compression');
+const helmet = require('helmet');
 const port = process.env.PORT || 3000;
 
 const livereload = require('livereload');
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, '/views'));
 liveReloadServer.watch(path.join(__dirname, 'public'));
-
 liveReloadServer.server.once('connection', () => {
   setTimeout(() => {
     liveReloadServer.refresh('/');
   }, 100);
 });
-
 const connectLivereload = require('connect-livereload');
+app.use(connectLivereload());
 
 // add handlebars helpers: https://stackoverflow.com/a/32707476
 const handlebars = require('./src/helpers/handlebars')(exphbs);
 
+app.use(compression());
+
+app.use(helmet());
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/views'));
@@ -37,8 +41,6 @@ app.use(
     'https://api.github.com/tluhk/HK_Riistvara-alused/contents/concepts/arvuti/images'
   )
 );
-
-app.use(connectLivereload());
 
 const { engine } = require('./engine');
 
