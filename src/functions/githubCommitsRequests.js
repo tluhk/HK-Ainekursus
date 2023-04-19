a6df82c4c550adf2fc2cf66b99ceaa7ba48895e1
Krister Sepp <kristersepp@gmail.com>
2023-04-19 19:12:26 +0300

    clean more files, add more comments

--- src/functions/githubCommitsRequests.js
+++ src/functions/githubCommitsRequests.js
@@ -4,20 +4,9 @@ const baseUrl = 'https://api.github.com';
 
 // Github API request endpoints
 const githubCommitsRequests = {
-  requestRepos: 'https://api.github.com/orgs/tluhk/repos',
-  requestTeamCourses: (
-    (teamSlug) => `${baseUrl}/orgs/tluhk/teams/${teamSlug}/repos`
-  ),
-  requestRepoBranches: (
-    (coursePathInGithub) => `${baseUrl}/repos/${coursePathInGithub}/branches`
-  ),
-  // config
-  requestConfig: (
-    (coursePathInGithub, refBranch) => `${baseUrl}/repos/${coursePathInGithub}/contents/config.json?ref=${refBranch}`
-  ),
-  // docs related
   requestCommits: (
     (coursePathInGithub, refBranch) => {
+      /** Request only commits added max 30 days ago */
       const date = new Date();
       date.setDate(date.getDate() - 30);
       const dateISO = date.toISOString();
