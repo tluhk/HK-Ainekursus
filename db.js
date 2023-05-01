f4d4e3d0f01d85c2333c5c9b91026f92c9c8fc35
Krister Sepp <kristersepp@gmail.com>
2023-05-01 14:27:29 +0300

    improve mysql speed in db.js

--- db.js
+++ db.js
@@ -1,17 +1,42 @@
 import mariadb from 'mariadb';
 // https://stackoverflow.com/a/74320569
 import dotenv from 'dotenv';
+// Load modules
+import PoolManager from 'mysql-connection-pool-manager';
 
 dotenv.config();
 
-const pool = mariadb.createPool({
+/* const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: 'course_management',
   connectionLimit: 5,
-  waitForConnections: true,
+  waitForConnections: false,
   trace: false,
-});
+  idleTimeout: 1
+}); */
+
+// Define mySQL settings
+const mySql = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'course_management',
+}
+
+// Pool manager settings
+const poolManager = {
+  idleCheckInterval: 1000,
+  maxConnextionTimeout: 30000,
+  idlePoolTimeout: 3000,
+  errorLimit: 5,
+  preInitDelay: 50,
+  sessionTimeout: 60000,
+  mySQLSettings: mySql
+}
+
+// Initialising the instance
+const pool = PoolManager(poolManager);
 
 export default pool;
