851d31d3e5347378db64e8a3f6e171b98abfe5bf
Juho Kalberg <juss@vilenkell.eu>
2023-05-22 18:14:13 +0300

    sidebars settings

--- public/js/js.js
+++ public/js/js.js
@@ -16,14 +16,15 @@ const panMainContentRight = () => {
 };
 
 // add class on mediaquery change
-const mediaQuery = window.matchMedia('(max-width: 1024px)');
+const mediaQuery = window.matchMedia('(min-width: 1024px)');
 function handleTabletChange(e) {
   // Check if the media query is true
   if (e.matches) {
-    let x = document.querySelector('.aside');
+    let x = document.querySelector('.closing-menu');
     x.classList.toggle('menu-zero');
   }
 }
+
 /* // Register event listener
 mediaQuery.addListener(handleTabletChange);
 
