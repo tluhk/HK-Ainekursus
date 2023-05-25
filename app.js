2657642cc46b9cf2d54149fca849451f06faa2e5
Juho Kalberg <juhokalberg@Juhos-MacBook-Pro-2.local>
2023-05-25 17:36:59 +0300

    dark n light theme pure css

--- app.js
+++ app.js
@@ -414,7 +414,7 @@ app.use(getTeamAssignments, async (req, res, next) => {
     // console.log('userTeam1:', userTeam);
     req.user.team = userTeam;
     // eslint-disable-next-line brace-style
-  } /* else {
+  } else {
 
   /**
    * TO ALLOW LOGGING IN WITH ANY USER, COMMENT OUT FOLLOWING else STATEMENT!
@@ -454,7 +454,7 @@ app.use(getTeamAssignments, async (req, res, next) => {
       // console.log('userTeam1:', userTeam);
       req.user.team = userTeam;
     }
-  } 
+  } /* */
     req.user = {
       id: '132268493',
       nodeId: 'U_kgDOB-JBzQ=',
@@ -483,7 +483,7 @@ app.use(getTeamAssignments, async (req, res, next) => {
       // console.log('userTeam1:', userTeam);
       req.user.team = userTeam;
     }
-  } */
+  } 
 
   next();
 });
