d0451d610373991b2099c18ce4c4eac90e3a769e
Juho Kalberg <juhokalberg@Juhos-MacBook-Pro-2.local>
2023-05-27 14:11:49 +0300

    notifications remake

---
+++ db.js
@@ -0,0 +1,24 @@
+import mariadb from 'mariadb';
+// https://stackoverflow.com/a/74320569
+import dotenv from 'dotenv';
+// Load modules
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'course_management',
+  idleCheckInterval: 1000,
+  maxConnextionTimeout: 30000,
+  idlePoolTimeout: 3000,
+  errorLimit: 5,
+  preInitDelay: 50,
+  sessionTimeout: 60000,
+  /* Setting acquireTimeout helps running app WITHOUT Docker quicker (npm run start-app).
+  Do NOT enable acquireTimeout on live site WITH Docker (npm start) – this will close database pools to quickly and DB actions will get ignored.  */
+  // acquireTimeout: 50, // acquireTimeout option defines the maximum number of milliseconds to wait for a connection to become available in the pool before throwing an error.
+});
+
+export default pool;
