0012a5fea4dcac08805e4674cb92a8ffd8dbc77f
Krister Herman Sepp <krister.sepp@ainekursus.kolledz>
2023-06-01 21:07:52 +0300

    Revert "added js compression tool"
    This reverts commit b5c959173b653d3cbad910ed745040887d5e210c.

--- public/js/dmswitch.js
+++ public/js/dmswitch.js
@@ -1,1 +1,9 @@
-window.onload=function(){var e=document.documentElement,t=document.getElementById("switch");e.classList.contains("light-theme")&&(t.checked=!1)};
+// check which theme is choosed and if it is dark then let the mode switcher will be selected
+window.onload = function () {
+  var htmlElement = document.documentElement;
+  var checkboxElement = document.getElementById('switch');
+
+  if (htmlElement.classList.contains('light-theme')) {
+    checkboxElement.checked = false;
+  }
+};
